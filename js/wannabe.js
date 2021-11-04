7c1d62652c210d42041cb613a41402e9bc39c71b
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-04 20:16:03 +0100

    esercizio 2

--- js/wannabe.js
+++ js/wannabe.js
@@ -1,1 +1,17 @@
-alert('Helloword')
+
+
+
+
+
+
+// Esercizio 2
+
+document.getElementById("nome").innerHTML =
+"pippo";
+document.getElementById("cognome").innerHTML =
+"rossi";
+document.getElementById("colore").innerHTML =
+"green";
+document.getElementById("21").innerHTML =
+"21";
+
